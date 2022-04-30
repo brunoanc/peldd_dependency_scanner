2fb23c839a19e8f3d0a59cda7fbc85351e02d792
piegames <git@piegames.de>
2022-04-30 11:37:05 +0200

    Move get_all_dependencies to associated method

--- src/main.rs
+++ src/main.rs
@@ -22,67 +22,73 @@ struct Args {
     full_path: bool,
 }
 
+impl Args {
+    pub fn get_all_dependencies(&self) -> Vec<String> {
+        self.get_all_dependencies_inner(&self.file)
+    }
+
+    fn get_all_dependencies_inner(&self, file: &str) -> Vec<String> {
+        if let Ok(mut a) = ACCESSED_FILES.lock() {
+            if a.contains(&file.to_string()) {
+                if self.verbose > 1 {
+                    println!("File {} already tested.", file);
+                }
+                return vec![];
+            } else {
+                a.push(file.to_string());
+            }
+        } else {
+            if self.verbose > 0 {
+                println!("Mutex has been poisoned!");
+            }
+            return vec![];
+        }
+
+        let filepath = if let Some(path) = get_filepath(file, &self.dll_path) {
+            path
+        } else {
+            if self.verbose > 0 {
+                println!("Couldn't find library {}", file);
+            }
+            return vec![];
+        };
+
+        if self.verbose > 0 {
+            println!("Checking file {:?}.", &filepath);
+        }
+        let mut f = vec![];
+        let mut files = Command::new("peldd");
+        let files = files.arg(&filepath);
+        if let Ok(out) = files.output() {
+            let string = String::from_utf8_lossy(&out.stdout);
+            for file in string.lines() {
+                if self.full_path {
+                    if let Some(a) = get_filepath(file, &self.dll_path) {
+                        f.push(a);
+                    } else {
+                        if self.verbose > 0 {
+                            println!("Couldn't find library {}", file);
+                        }
+                        continue;
+                    }
+                } else {
+                    f.push(file.to_string());
+                }
+                f.append(&mut self.get_all_dependencies_inner(file));
+            }
+        }
+        f
+    }
+}
+
 fn main() {
     let args = Args::parse();
-    let mut deps = get_all_dependencies(&args, &args.file);
+    let mut deps = args.get_all_dependencies();
     deps.sort();
     deps.dedup();
     for v in deps {
         println!("{}", v);
     }
-}
-
-fn get_all_dependencies(args: &Args, file: &str) -> Vec<String> {
-    if let Ok(mut a) = ACCESSED_FILES.lock() {
-        if a.contains(&file.to_string()) {
-            if args.verbose > 1 {
-                println!("File {} already tested.", file);
-            }
-            return vec![];
-        } else {
-            a.push(file.to_string());
-        }
-    } else {
-        if args.verbose > 0 {
-            println!("Mutex has been poisoned!");
-        }
-        return vec![];
-    }
-
-    let filepath = if let Some(path) = get_filepath(file, &args.dll_path) {
-        path
-    } else {
-        if args.verbose > 0 {
-            println!("Couldn't find library {}", file);
-        }
-        return vec![];
-    };
-
-    if args.verbose > 0 {
-        println!("Checking file {:?}.", &filepath);
-    }
-    let mut f = vec![];
-    let mut files = Command::new("peldd");
-    let files = files.arg(&filepath);
-    if let Ok(out) = files.output() {
-        let string = String::from_utf8_lossy(&out.stdout);
-        for file in string.lines() {
-            if args.full_path {
-                if let Some(a) = get_filepath(file, &args.dll_path) {
-                    f.push(a);
-                } else {
-                    if args.verbose > 0 {
-                        println!("Couldn't find library {}", file);
-                    }
-                    continue;
-                }
-            } else {
-                f.push(file.to_string());
-            }
-            f.append(&mut get_all_dependencies(args, file));
-        }
-    }
-    f
 }
 
 fn get_filepath(file: &str, dll_path: &str) -> Option<String> {
